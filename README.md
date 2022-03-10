9b247bc22ec6763386142fa7e8af1ee9734ed5b6
Caitlin Stevenson <caitlinstevenson@Caitlins-Air.attlocal.net>
2022-03-10 08:38:15 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# code_quiz
+# Caitlin Stevenson|Code Quiz Assignment
+
+## Objectives
+
+In this project, I...
+
+## Screenshot of Final Page
+
+![Screenshot of final webpage](./assets/images/PENDING)
+
+## Link To The Final Page
+
+[Link to final webpage](PENDING)
+
+## License
+
+This code is licensed under the MIT License.
---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Will click a button labeled "START" to open quiz
+
+// When button clicked a timer should start(displayed)
+  //and first question displayed
+
+//Question answered > Present next question > etc.
+
+//When question answered incorrectly time should be deducted from the timer.
+
+//Quiz is over when all questions are answered or timer hits 0
+
+//When quiz is over user can save their initials and score.
